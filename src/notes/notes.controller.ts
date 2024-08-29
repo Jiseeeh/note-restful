40bae08664fe333f4bcdea6a615f787ff0f290a5
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-29 22:09:15 +0800

    feat: add crud impl

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,30 +1,68 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 import { GoogleOAuthGuard } from 'src/auth/guards/google-oauth.guard';
 import { NotesService } from './notes.service';
-import { Note } from './interfaces/note.interface';
-import { CreateNoteDto } from './dto/create-note-dto';
+import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
 import { ValidationPipe } from './pipes/validation.pipe';
+import { Note } from './schemas/note.schema';
 
 @Controller('notes')
 @UseGuards(GoogleOAuthGuard)
 export class NotesController {
   constructor(private notesService: NotesService) {}
 
-  @Get()
-  async findAll(@Req() req): Promise<Note[]> {
-    return this.notesService.findAllByUser({
-      email: req.user.email,
-    });
-  }
-
   @Post()
   async create(
     @Req() req,
     @Body(new ValidationPipe()) createNoteDto: CreateNoteDto,
   ): Promise<Note> {
-    // i did not include the user email in the createNoteDto as
-    // it will fail in validation because it runs first before i have the user email from req
     return this.notesService.create(createNoteDto, req.user.email);
+  }
+
+  @Get()
+  async findAll(@Req() req): Promise<Note[]> {
+    return this.notesService.findAllByUser({
+      userEmail: req.user.email,
+    });
+  }
+
+  @Get(':noteId')
+  async findOne(@Req() req, @Param('noteId') noteId: string): Promise<Note> {
+    return this.notesService.findOne({
+      id: noteId,
+      userEmail: req.user.email,
+    });
+  }
+
+  @Patch(':noteId')
+  async updateOne(
+    @Req() req,
+    @Param('noteId') noteId: string,
+    @Body(new ValidationPipe()) updateNoteDto: UpdateNoteDto,
+  ) {
+    return this.notesService.updateOne({
+      id: noteId,
+      userEmail: req.user.email,
+      updateNoteDto,
+    });
+  }
+
+  @Delete(':noteId')
+  async deleteOne(@Req() req, @Param('noteId') noteId: string) {
+    return this.notesService.deleteOne({
+      id: noteId,
+      userEmail: req.user.email,
+    });
   }
 }
