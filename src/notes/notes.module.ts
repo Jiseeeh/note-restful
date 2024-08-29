cd037691ffd860d4dfe2ee123118a502ead366ea
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-29 22:08:49 +0800

    feat: import mongoose in notes module

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
+import { NoteRepository } from './note.repository';
+import { Note, NoteSchema } from './schemas/note.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
+  ],
   controllers: [NotesController],
-  providers: [NotesService],
+  providers: [NotesService, NoteRepository],
 })
 export class NotesModule {}
