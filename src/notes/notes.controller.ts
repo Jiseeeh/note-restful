fefbdb089a7cec56968cdb36f0fb3cab26a41c4a
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-29 18:05:32 +0800

    feat: add  temp impl

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 
+import { GoogleOAuthGuard } from 'src/auth/guards/google-oauth.guard';
 import { NotesService } from './notes.service';
 import { Note } from './interfaces/note.interface';
+import { CreateNoteDto } from './dto/create-note-dto';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 @Controller('notes')
+@UseGuards(GoogleOAuthGuard)
 export class NotesController {
   constructor(private notesService: NotesService) {}
 
-  @Post()
-  async create(): Promise<Note> {
-    return this.notesService.create();
+  @Get()
+  async findAll(@Req() req): Promise<Note[]> {
+    return this.notesService.findAllByUser({
+      email: req.user.email,
+    });
   }
 
-  @Get()
-  async findAll(): Promise<Note[]> {
-    return this.notesService.findAll();
+  @Post()
+  async create(
+    @Req() req,
+    @Body(new ValidationPipe()) createNoteDto: CreateNoteDto,
+  ): Promise<Note> {
+    // i did not include the user email in the createNoteDto as
+    // it will fail in validation because it runs first before i have the user email from req
+    return this.notesService.create(createNoteDto, req.user.email);
   }
 }
